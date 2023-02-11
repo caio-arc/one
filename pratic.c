155be07d5502e25d886eba50f087d2709afe05f6
caio-arc <caioarcos99@gmail.com>
2023-02-11 10:39:15 -0400

    praticar
    ling c

---
+++ pratic.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main () {
+	
+	float valor1, valor2, soma;
+	
+	printf (" digite o primeira valor:\n");
+	scanf("%f",&valor1);
+	
+	printf (" digite o segundo valor:\n");
+	scanf("%f",&valor2);
+	
+	soma = valor1 + valor2;
+	
+	printf("%f", soma);
+
+}
+
